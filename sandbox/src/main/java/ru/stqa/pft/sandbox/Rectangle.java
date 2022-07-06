09c36eb706306183fe6c5647df410f896fb9a842
anatolyhalak <108192896+anatolyhalak@users.noreply.github.com>
2022-07-07 00:30:34 +0300

    Классы и объекты
    Создание классов и объектов, а также конструкторов

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class Rectangle {
+
+    public double a;
+    public double b;
+
+    public Rectangle (double a, double b){
+        this.a = a;
+        this.b = b;
+    }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+
+    public double l;
+
+    public Square (double l){
+        this.l = l;
+    }
+}
