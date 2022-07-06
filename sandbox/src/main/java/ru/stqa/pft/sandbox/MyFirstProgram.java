c123211e88f29b5b9a50f13e8f18343b43e6bedf
anatolyhalak <108192896+anatolyhalak@users.noreply.github.com>
2022-07-06 23:27:09 +0300

    Переменные и значения
    Написан пример литеральных значений и вычислительных значений

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,10 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] agrs) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello," +  "world!");
+
+        int l = 8; //литеральные значения - явно описаны в коде программы
+        int s = l * l; //вычислимые значения - результат арифметической операции
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 }
